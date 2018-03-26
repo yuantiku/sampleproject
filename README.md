cf5b823e8cfadb8c65d3da07e62307cba4ddf330
zhangyc <zhangyc@fenbi.com>
2018-03-26 15:32:13 +0800

    ADD: Markdown readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# A Python Library for students in Yuanfudao
+
+[![Build Status](https://travis-ci.org/yuantiku/ybckit.svg?branch=master)](https://travis-ci.org/yuantiku/ybckit)
+
+## License
+
+MIT.
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -21,7 +21,9 @@ setup(
 
     description='Python library kit for education with yuanfudao.',  # Required
 
-    long_description=long_description,  # Optional
+    long_description=long_description,
+
+    long_description_content_type='text/markdown',
 
     url='https://github.com/yuantiku/ybckit',  # Optional
 
