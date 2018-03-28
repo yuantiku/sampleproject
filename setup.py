478156683109b9ca69f0d1d992b254e0fd7de5ab
zhangyc <zhangyc@fenbi.com>
2018-03-28 12:37:55 +0800

    Bump version: 0.0.22 → 0.0.23

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='ybckit',  # Required
 
-    version='0.0.22',
+    version='0.0.23',
 
     description='Python library kit for education with yuanfudao.',
 
