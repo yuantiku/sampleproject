c619deb944cade267897487dc8dd78697f38250f
skyline75489 <skyline75489@outlook.com>
2018-04-11 11:23:21 +0800

    MOD: raise log level to warning when in YBC_ENV

--- ybckit/log.py
+++ ybckit/log.py
@@ -6,13 +6,15 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import os
-from logging import DEBUG, INFO, basicConfig
-
+from logging import DEBUG, INFO, WARNING, basicConfig
 
 def init():
     if 'YBC_DEBUG' in os.environ:
         logging_level = DEBUG
     else:
         logging_level = INFO
+
+    if 'YBC_ENV' in os.environ:
+        logging_level = WARNING
 
     basicConfig(level=logging_level)
