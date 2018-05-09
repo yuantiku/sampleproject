afb52b2a55164f3b5ec099ceaddcbcbcd787b3da
skyline75489 <skyline75489@outlook.com>
2018-05-09 12:37:33 +0800

    MOD: init as needed

--- ybckit/runner.py
+++ ybckit/runner.py
@@ -6,12 +6,10 @@ from __future__ import unicode_literals
 
 import logging
 import sys
+import ast
 
 from .config import YBC_CONFIG
-from .gui import init as gui_init
 from .log import init as logging_init
-from .media import init as media_init
-from .mpl import init as mpl_init
 
 logger = logging.getLogger(__name__)
 
@@ -32,6 +30,33 @@ class Unbuffered(object):
         return getattr(self.stream, attr)
 
 
+def init_if_needed(source_file):
+    all_imports = set()
+
+    with open(source_file) as f:
+        ast_tree = ast.parse(f.read())
+
+    for statement in ast_tree.body:
+        if isinstance(statement, ast.Import):
+            for _import in statement.names:
+                all_imports.update(_import.name.split('.'))
+
+    if 'matplotlib' in all_imports:
+        from .mpl import init as mpl_init
+        logger.debug('import mpl')
+        mpl_init()
+
+    if 'easygui' in all_imports or 'ybc_box' in all_imports:
+        from .gui import init as gui_init
+        logger.debug('import gui')
+        gui_init()
+
+    if 'ybc_speech' in all_imports:
+        from .media import init as media_init
+        logger.debug('import media')
+        media_init()
+
+
 if __name__ == '__main__':
     logging_init()
 
@@ -42,14 +67,11 @@ if __name__ == '__main__':
     sys.stdout = Unbuffered(sys.stdout)
     sys.stderr = Unbuffered(sys.stderr)
 
-    mpl_init()
-    gui_init()
-    media_init()
-
     if len(sys.argv) <= 1:
         logger.error('参数错误，需指定一个 script 来运行，例如 python -m ybckit.runner hello.py')
         sys.exit(1)
 
     script_file = sys.argv[1]
+    init_if_needed(script_file)
 
     exec(open(script_file, 'rb').read())
