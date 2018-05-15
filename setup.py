afcae793598fe941bc310bca32bcc54962967428
zhangyc <zhangyc@fenbi.com>
2018-05-15 10:33:38 +0800

    Bump version: 0.0.52 → 0.0.53

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='ybckit',  # Required
 
-    version='0.0.52',
+    version='0.0.53',
 
     description='Python library kit for education with yuanfudao.',
 
